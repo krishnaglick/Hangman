56078cdd85ee4b4b1ec09a0206a9b3ed1a22f991
Krishna Glick <Krishna.Glick@starfieldtms.com>
2014-02-21 13:55:12 -0500

    First

---
+++ Hangman/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace Hangman
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxUserInput = new System.Windows.Forms.TextBox();
+            this.labelTextboxInstructions = new System.Windows.Forms.Label();
+            this.labelWord = new System.Windows.Forms.Label();
+            this.pictureBoxHangman = new System.Windows.Forms.PictureBox();
+            this.labelShowWord = new System.Windows.Forms.Label();
+            this.labelListOfBadLetters = new System.Windows.Forms.Label();
+            this.labelLetterTrackerText = new System.Windows.Forms.Label();
+            this.buttonSubmitLetter = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxHangman)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxUserInput
+            // 
+            this.textBoxUserInput.Location = new System.Drawing.Point(326, 194);
+            this.textBoxUserInput.MaxLength = 1;
+            this.textBoxUserInput.Name = "textBoxUserInput";
+            this.textBoxUserInput.Size = new System.Drawing.Size(100, 20);
+            this.textBoxUserInput.TabIndex = 0;
+            this.textBoxUserInput.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxUserInput_KeyPress);
+            // 
+            // labelTextboxInstructions
+            // 
+            this.labelTextboxInstructions.AutoSize = true;
+            this.labelTextboxInstructions.Location = new System.Drawing.Point(238, 197);
+            this.labelTextboxInstructions.Name = "labelTextboxInstructions";
+            this.labelTextboxInstructions.Size = new System.Drawing.Size(82, 13);
+            this.labelTextboxInstructions.TabIndex = 1;
+            this.labelTextboxInstructions.Text = "Choose a Letter";
+            // 
+            // labelWord
+            // 
+            this.labelWord.AutoSize = true;
+            this.labelWord.Location = new System.Drawing.Point(247, 143);
+            this.labelWord.Name = "labelWord";
+            this.labelWord.Size = new System.Drawing.Size(33, 13);
+            this.labelWord.TabIndex = 2;
+            this.labelWord.Text = "Word";
+            // 
+            // pictureBoxHangman
+            // 
+            this.pictureBoxHangman.Location = new System.Drawing.Point(12, 12);
+            this.pictureBoxHangman.Name = "pictureBoxHangman";
+            this.pictureBoxHangman.Size = new System.Drawing.Size(220, 231);
+            this.pictureBoxHangman.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBoxHangman.TabIndex = 3;
+            this.pictureBoxHangman.TabStop = false;
+            // 
+            // labelShowWord
+            // 
+            this.labelShowWord.AutoSize = true;
+            this.labelShowWord.Location = new System.Drawing.Point(247, 156);
+            this.labelShowWord.Name = "labelShowWord";
+            this.labelShowWord.Size = new System.Drawing.Size(43, 13);
+            this.labelShowWord.TabIndex = 4;
+            this.labelShowWord.Text = "______";
+            // 
+            // labelListOfBadLetters
+            // 
+            this.labelListOfBadLetters.AutoSize = true;
+            this.labelListOfBadLetters.Location = new System.Drawing.Point(358, 156);
+            this.labelListOfBadLetters.Name = "labelListOfBadLetters";
+            this.labelListOfBadLetters.Size = new System.Drawing.Size(0, 13);
+            this.labelListOfBadLetters.TabIndex = 5;
+            // 
+            // labelLetterTrackerText
+            // 
+            this.labelLetterTrackerText.AutoSize = true;
+            this.labelLetterTrackerText.Location = new System.Drawing.Point(358, 143);
+            this.labelLetterTrackerText.Name = "labelLetterTrackerText";
+            this.labelLetterTrackerText.Size = new System.Drawing.Size(61, 13);
+            this.labelLetterTrackerText.TabIndex = 6;
+            this.labelLetterTrackerText.Text = "Bad Letters";
+            // 
+            // buttonSubmitLetter
+            // 
+            this.buttonSubmitLetter.Location = new System.Drawing.Point(351, 220);
+            this.buttonSubmitLetter.Name = "buttonSubmitLetter";
+            this.buttonSubmitLetter.Size = new System.Drawing.Size(75, 23);
+            this.buttonSubmitLetter.TabIndex = 7;
+            this.buttonSubmitLetter.Text = "Submit";
+            this.buttonSubmitLetter.UseVisualStyleBackColor = true;
+            this.buttonSubmitLetter.Click += new System.EventHandler(this.buttonSubmitLetter_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.buttonSubmitLetter;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 251);
+            this.Controls.Add(this.buttonSubmitLetter);
+            this.Controls.Add(this.labelLetterTrackerText);
+            this.Controls.Add(this.labelListOfBadLetters);
+            this.Controls.Add(this.labelShowWord);
+            this.Controls.Add(this.pictureBoxHangman);
+            this.Controls.Add(this.labelWord);
+            this.Controls.Add(this.labelTextboxInstructions);
+            this.Controls.Add(this.textBoxUserInput);
+            this.Name = "Form1";
+            this.Text = "Hang-A-Bro";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxHangman)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxUserInput;
+        private System.Windows.Forms.Label labelTextboxInstructions;
+        private System.Windows.Forms.Label labelWord;
+        private System.Windows.Forms.PictureBox pictureBoxHangman;
+        private System.Windows.Forms.Label labelShowWord;
+        private System.Windows.Forms.Label labelListOfBadLetters;
+        private System.Windows.Forms.Label labelLetterTrackerText;
+        private System.Windows.Forms.Button buttonSubmitLetter;
+    }
+}
+
---
+++ Hangman/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hangman
+{
+    public partial class Form1 : Form
+    {
+
+        #region Variables
+        private String[] WordBank = { "POTATO", "CHEESE", "ZOMBIE", "MEMBER" };
+        private String currentWord;
+        private int numOfFails = 0;
+        #endregion
+
+        #region Initialize
+        public Form1()
+        {
+            InitializeComponent();
+            pickWord();
+            pictureBoxHangman.ImageLocation = "../../Images/" + ++numOfFails + ".jpg";
+        }
+
+        private void pickWord()
+        {
+            currentWord = WordBank[new Random().Next(WordBank.Length)];
+        }
+
+        #endregion
+
+        #region Only Allow Letters In Text Box
+        private void textBoxUserInput_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !(char.IsLetter(e.KeyChar) || e.KeyChar == (char)Keys.Back);
+        }
+        #endregion
+
+        #region Check If The Entered Letter Is Within The Chosen String
+        private void buttonSubmitLetter_Click(object sender, EventArgs e)
+        {
+            //If text box is blank, clear
+            if (textBoxUserInput.Text == "")
+            {
+                MessageBox.Show("Please choose a letter!");
+                return;
+            }
+
+            char testWord = textBoxUserInput.Text.ToUpper().ToCharArray()[0];
+            
+            //Don't want the user trying a letter multiple times!
+            if (labelListOfBadLetters.Text.Contains(testWord) || labelShowWord.Text.Contains(testWord))
+            {
+                textBoxUserInput.Clear();
+                textBoxUserInput.Focus();
+                return;
+            }
+
+            //If the word has what we want
+            if (currentWord.Contains(testWord))
+            {
+                //Look through the word to find all matches
+                for(int i = 0; i < currentWord.Length; i++)
+                {
+                    //If a match, replace the character at the index with the correct letter.
+                    if (currentWord[i] == testWord)
+                    {
+                        StringBuilder sb = new StringBuilder(labelShowWord.Text);
+                        sb[i] = testWord;
+                        labelShowWord.Text = sb.ToString();
+                    }
+                }
+            }
+            //Word does not have what we want
+            else
+            {
+                //If the label is blank, set the letter, otherwise
+                if (labelListOfBadLetters.Text == "")
+                    labelListOfBadLetters.Text = testWord.ToString();
+                //Add the letter to the label
+                else
+                    labelListOfBadLetters.Text += ", " + testWord;
+                //Increment the hangman!
+                pictureBoxHangman.ImageLocation = "../../Images/" + ++numOfFails + ".jpg";
+            }
+            textBoxUserInput.Clear();
+            textBoxUserInput.Focus();
+
+            //Check for win condition
+            if(labelShowWord.Text == currentWord)
+            {
+                resetGame("You won! Play again?", "Win");
+            }
+
+            //Check for lose condition
+            if(numOfFails > 6)
+            {
+                resetGame("You lost! Play again?", "Lose");
+            }
+        }
+        #endregion
+
+        #region Reset The Game
+        private void resetGame(String message, String header)
+        {
+            //Create a message box and ask the user to play again.
+            DialogResult playAgain = MessageBox.Show(message,header, MessageBoxButtons.YesNo);
+            //If the user says Yes, reset the game.
+            if (playAgain == System.Windows.Forms.DialogResult.Yes)
+            {
+                numOfFails = 0;
+                pictureBoxHangman.ImageLocation = "../../Images/" + ++numOfFails + ".jpg";
+                labelShowWord.Text = "______";
+                labelListOfBadLetters.Text = "";
+                pickWord();
+            }
+            //If no, end!
+            else
+            {
+                MessageBox.Show("Thanks for playing!");
+                Application.Exit();
+            }
+        }
+        #endregion
+
+    }
+}
